53fe968a3b5f1f8726bf5545df0330015ce7847f
hawks70 <alfonsofalcomata57@gmail.com>
2025-07-04 15:05:12 +0200

    Initial commit

---
+++ src/app/lib/firebase.js
@@ -0,0 +1,19 @@
+// lib/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdYoHrYI9zQdxhQOdaVT7o1lIQg8SRwAg",
+  authDomain: "attendance-2980e.firebaseapp.com",
+  projectId: "attendance-2980e",
+  storageBucket: "attendance-2980e.appspot.com",
+  messagingSenderId: "545038671009",
+  appId: "1:545038671009:web:05cd8f1490cf7d624164e0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
